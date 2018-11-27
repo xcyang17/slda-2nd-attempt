9f5b639ac9536708781d317f06033a07a99d80b9
xcyang <xcyang@berkeley.edu>
2018-11-27 02:56:58 -0500

    wanted to run code, but have indentation error, which is extremely stupid

--- news_category/gibbs-sampler-unsupervised.py
+++ news_category/gibbs-sampler-unsupervised.py
@@ -96,11 +96,76 @@ beta = np.ones((1, T))
 ## TODO: np.where returns weird result
 
 # Gibbs sampler
-L = 5000 # number of iterations
+#L = 5000 # number of iterations
 L = 5
+a = 1 # entry in alpha
+b = 1 # entry in beta
 
 # store the estimated \theta_{term, topic} and \phi_{doc, topic}
 theta_sample = np.zeros((T, K ,L))
+phi_sample = np.zeros((D, K, L))
+
+start_1 = timeit.default_timer()
+for l in range(L):
+    for d in range(D): # doc level
+    news_id = docs_txt_lines2[d]
+    tmp = j_array[np.where(i_array == news_id_dict[news_id])] # as `lst` in original pseudo code
+    Wd = list(itemgetter(*tmp.tolist())(terms_txt_lines2))
+    Nd = len(Wd)
+    for i in range(Nd): # term level
+        term = Wd[i]
+        term_id = term_id_dict[term]
+        Idi = len(doc_term_dict[(news_id, term)])
+        for j in range(Idi): # replicates of the term
+            k_hat = doc_term_dict[(news_id, term)][j]
+            doc_topic_mat[int(news_id), k_hat] -= 1 # C_{d, \hat{k}} -= 1
+            term_topic_mat[term_id, k_hat] -= 1 # C_{v, \hat{k}} -= 1
+            pks = []            
+            for k in range(K):
+                pks += [((doc_topic_mat[int(news_id), k] + 
+                        alpha[0,k])*(term_topic_mat[term_id, k]+beta[0,k]))/(topic_mat[0,k]+beta[0,k]*T)]
+            # then normalize
+            norm_cnst = sum(pks)
+            k_sampled = np.random.choice(K, 1, p = pks/norm_cnst)[0]
+            doc_topic_mat[int(news_id), k_sampled] += 1
+            term_topic_mat[term_id, k_sampled] += 1
+            #TODO: ???is topic_mat updated? or is n_{topic} not used in the sampling algo at all?
+            doc_term_dict[(news_id, term)][j] = k_sampled
+    # update phi (see pg.73 of http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf)
+    # this phi_{d,k} here looks like \theta_{d,z} in U Guleph tutorial
+    for d1 in range(D):
+        news_id1 = docs_txt_lines2[d1]
+        sum_C_d_k = sum(doc_topic_mat[int(news_id1),:])
+        phi_sample[int(news_id1),:,l] = (doc_topic_mat[int(news_id1),:] + a)/(sum_C_d_k + K*a)        
+    # update theta (see pg.73 of http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf)
+    # this theta_{v,k} here looks like \phi_{w,z} (= \phi_{z,w}) in U Guleph tutorial
+    for k1 in range(K):
+        sum_C_v_k = sum(term_topic_mat[:,k1])
+        theta_sample[:,k1,l] = (term_topic_mat[:,k1] + b)/(sum_C_v_k + T*b)
+
+stop_1 = timeit.default_timer()    
+print('Time: ', stop_1 - start_1) 
+    
+    
+        
+        
+#        term1 = terms_txt_lines2[t1]
+#        term1_id = term_id_dict[term1]
+#        sum_C_v_k = sum()
+        
+        
+#        tmp1 = j_array[np.where(i_array == news_id_dict[news_id1])] # as `lst` in original pseudo code
+#        Wd1 = list(itemgetter(*tmp1.tolist())(terms_txt_lines2))
+#        Nd1 = len(Wd1)
+#        d1_term_ids = [term_id_dict[each] for each in Wd1]
+#        sum_C_v_k = sum(term_topic_mat[int(news_id1),:])
+#        for k1 in range(K):
+            
+        
+#        phi_sample[int(news_id1),:,l] = (doc_topic_mat[int(news_id1),:] + a)/(sum_C_d_k + K*a)
+        
+            
+            
 
 
 start_1 = timeit.default_timer()
