cde16b8bf4f1dd109a37b6a14238851c3a53c827
ccui5 <ccui5@ncsu.edu>
2018-11-29 22:12:45 -0500

    Add files via upload

---
+++ news_category/ecgs-sLDA-keys.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 12:44:04 2018
+
+@author: apple
+"""
+
+# TODO: write efficient CGS as in figure 3 of http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf
+
+# import packages
+import numpy as np
+import timeit
+from collections import Counter
+from operator import itemgetter
+import random
+import math
+import pandas as pd
+
+# load R output
+working_dir = "/Users/CuiCan/Desktop/Slides/NCState/ST 740/Final Project New/ST740-FA18-Final/news_category/R_output_3/"
+i_txt = open(working_dir + "i-3.txt", "r")
+i_txt_lines = i_txt.readlines()
+i_txt_lines2 = [line.rstrip('\n') for line in i_txt_lines] # remove newlines '\n'
+i_r = list(map(int, i_txt_lines2)) # turn the list of strings into a list of ints
+i_array = np.asarray(i_r)
+
+j_txt = open(working_dir + "j-3.txt", "r")
+j_txt_lines = j_txt.readlines()
+j_txt_lines2 = [line.rstrip('\n') for line in j_txt_lines] # remove newlines '\n'
+j_r = list(map(int, j_txt_lines2))
+j_array = np.asarray(j_r)
+
+v_txt = open(working_dir + "v-3.txt", "r")
+v_txt_lines = v_txt.readlines()
+v_txt_lines2 = [line.rstrip('\n') for line in v_txt_lines] # remove newlines '\n'
+v_r = list(map(int, v_txt_lines2))
+v_array = np.asarray(v_r)
+
+terms_txt = open(working_dir + "Terms-3.txt", "r")
+terms_txt_lines = terms_txt.readlines()
+terms_txt_lines2 = [line.rstrip('\n') for line in terms_txt_lines] # remove newlines '\n'
+terms_txt_lines2 = [line.replace('"', '') for line in terms_txt_lines2] # remove double quotes
+
+docs_txt = open(working_dir + "Docs-3.txt", "r")
+docs_txt_lines = docs_txt.readlines()
+docs_txt_lines2 = [line.rstrip('\n') for line in docs_txt_lines] # remove newlines '\n'
+# ^^^ keep news_id as character instead of number
+docs_r = list(map(int, docs_txt_lines2))
+docs_array = np.asarray(docs_r)
+
+# Include label
+label_txt = open(working_dir + "label.txt", "r")
+label_txt_lines = label_txt.readlines()
+label_txt_lines2 = [line.rstrip('\n') for line in label_txt_lines]
+label_txt_lines2 = [line.replace('"','') for line in label_txt_lines2]
+
+
+r_output = [i_r, j_r, v_r, terms_txt_lines2, docs_r, label_txt_lines2]
+
+# Check if correctly read in
+for i in range(6):
+    print(r_output[i][0:10])
+
+# initializing the \bar{Z} at random from a uniform multinomial from (1, ..., K = 31)
+K = 31 # number of topics
+doc_term_dict = dict()
+doc_term_dict_R31 = dict() # add this dictionary to store the R^{31} form representation of doc_term_dict
+doc_topic_mat = np.zeros((len(docs_r), K))
+term_topic_mat = np.zeros((len(terms_txt_lines2), K))
+topic_mat = np.zeros((1, K))
+
+random.seed(9)
+
+start = timeit.default_timer()
+for idx in range(len(j_r)):
+    news_id = docs_txt_lines2[i_r[idx]] # i_r[idx] as id for `doc`
+    term = terms_txt_lines2[j_r[idx]] # j_r[idx] as id for `term`
+    k = (news_id, term)
+    val_len = v_r[idx]
+    doc_term_dict[k] = np.random.choice(K, val_len)
+    # store the n_{doc, topic}, n_{term, topic} and n_{topic}
+    freq = Counter(doc_term_dict[k])
+    tmp_dict=dict(freq)
+    tmp_array = np.array(list(tmp_dict.items()))
+    tmpp = np.zeros((1, K))
+    tmpp[0,tmp_array[:,0]] = tmp_array[:,1]
+    doc_term_dict_R31[k] = tmpp    
+    for existent_topic in list(freq.keys()):
+        ## update n_{doc, topic}
+        # (d, k)-th entry in n_{doc, topic} = 
+        # number of times the k-th topic being assigned to terms in 
+        # the d-th document (including replicates)
+        doc_topic_mat[int(news_id), existent_topic] += freq[existent_topic]
+        ## update n_{term, topic}
+        # (w,k)-th entry in n_{term, topic} = 
+        # number of times the w-th term being assigned to the k-th topic
+        # acorss all D documents
+        term_topic_mat[j_r[idx], existent_topic] += freq[existent_topic]
+        ## update n_{topic}
+        # k-th entry in n_{topic} = number of times the k-th topic
+        # is being assigned to a term across all D documents
+        topic_mat[0,existent_topic] += freq[existent_topic]
+
+stop = timeit.default_timer()    
+print('Time: ', stop - start) # 53.0269021549999
+
+# define dictionaries for term_id and news_id
+T = len(terms_txt_lines2)
+D = len(docs_r)
+term_id_dict = dict(zip(terms_txt_lines2, range(len(terms_txt_lines2))))
+news_id_dict = dict(zip(docs_txt_lines2, range(len(docs_txt_lines2))))
+# Add dictionaries for label_id
+freq = Counter(label_txt_lines2)
+label_id_dict = dict(zip(freq.keys(),range(31)))
+doc_label_dict = dict()
+for i in range(len(docs_txt_lines2)):
+    doc_label_dict[docs_txt_lines2[i]] = label_txt_lines2[i]
+
+alpha = np.ones((1, K))
+beta = np.ones((1, T))
+
+# save the original matrix
+doc_topic_mat_orig = doc_topic_mat
+term_topic_mat_orig = term_topic_mat
+topic_mat_orig = topic_mat
+doc_term_dict_R31_orig = doc_term_dict_R31
+doc_term_dict_orig = doc_term_dict
+
+
+doc_topic_mat = doc_topic_mat_orig
+term_topic_mat = term_topic_mat_orig
+topic_mat = topic_mat_orig
+doc_term_dict_R31 = doc_term_dict_R31_orig
+doc_term_dict = doc_term_dict_orig
+
+# Define function to update eta
+def eta_gradient(k,n_sample=10,s=1111):
+    gradient = np.zeros(K)
+    random.seed(s)
+    subsample = np.random.choice(range(D),n_sample,replace=False)
+    for d in subsample:
+        news_id = docs_txt_lines2[d]
+        zbar = doc_topic_mat[int(news_id),:]/sum(doc_topic_mat[int(news_id),:])
+        Pd = np.zeros((K,1))
+        Yd = np.zeros((K,1))
+        label = doc_label_dict[news_id]
+        Yd[label_id_dict[label],0] = 1
+        Pd = np.exp(np.dot(eta,zbar))
+        Pd = Pd / sum(Pd)
+        gradient = gradient + zbar*(Pd[k] - Yd[k,0])
+    const = lam * eta[k,:]
+    const = const.reshape((const.shape[0],))
+    gradient = gradient/len(subsample) + const
+    eta[k,:] = eta[k,:] + gradient * step
+    return [gradient,subsample, eta]
+
+# Define function to calculate loss for each iteration of updading eta
+def loss_f(times):
+    loss = []
+    for t in range(times):
+        for j in range(31):
+            subsample = eta_gradient(j)[1]
+            Ld = np.zeros(1)
+            for d1 in subsample:
+                news_id = docs_txt_lines2[d1]
+                zbar = doc_topic_mat[int(news_id),:]/sum(doc_topic_mat[int(news_id),:])
+                Pd = np.zeros((K,1))
+                Yd = np.zeros((K,1))
+                label = doc_label_dict[news_id]
+                Yd[label_id_dict[label],0] = 1
+                Pd = np.exp(np.dot(eta,zbar))
+                Pd = Pd / sum(Pd)
+                Ld = Ld - math.log(Pd[label_id_dict[label]])
+            tmp = sum(np.linalg.norm(eta,axis=1)**2)/2
+            loss.extend (Ld/len(subsample) + lam * tmp)
+    return loss
+
+
+# Implementation of Figure 2 in http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf
+# Gibbs sampler
+MCMC_iters = 1 # number of iterations
+a = 0.1 # entry in alpha
+b = 0.1 # entry in beta
+
+# define parms need in stochastic gradient descent
+eta = np.zeros([K,K]) + 1/K #inital eta
+lam = 0.5 # tunning parm
+step = 1 #step size
+
+# store the estimated \theta_{term, topic} and \phi_{doc, topic}
+theta_sample = np.zeros((T, K ,MCMC_iters))
+phi_sample = np.zeros((D, K, MCMC_iters))
+update = 5 # threthold to update eta
+count = 0 # record the iteration for upd
+seed = 1110 # set seed
+
+start_1 = timeit.default_timer()
+for m in range(MCMC_iters):
+    start_loop1 = timeit.default_timer()
+    print(m)
+    for (key,value) in doc_term_dict.items():
+        count += 1
+        news_id=key[0]
+        term=key[1]
+        term_id=term_id_dict[term]
+        Ndi=len(value)
+        
+        zbar_f = doc_topic_mat[int(news_id),:]/sum(doc_topic_mat[int(news_id),:])
+        doc_topic_mat[int(news_id),:] = doc_topic_mat[int(news_id),:] - doc_term_dict_R31[(news_id, term)]
+        term_topic_mat[term_id, :] = term_topic_mat[term_id, :] - doc_term_dict_R31[(news_id, term)]
+        topic_mat[0,:] = topic_mat[0,:] - doc_term_dict_R31[(news_id, term)]
+        zbar_r = doc_topic_mat[int(news_id),:]/sum(doc_topic_mat[int(news_id),:])
+        
+        if (count % 5000 < 0): # update eta per 5000 iteration, about 300 times
+            print(count)
+            seed += 1
+            for k in range(K):
+                eta = eta_gradient(k,s=seed)[2] 
+                # seed=1111 seems not work here, use different 10 docs to update eta_i
+                # use the zbar before deleting to update eta
+                # use the same docs to update eta_i or different docs????
+                # now use the same docs for whole updating of eta KxK matrix
+            label = doc_label_dict[news_id]
+            Pd_f = np.exp(np.dot(eta,zbar_f))
+            Pdk_f = Pd_f[label_id_dict[label]]
+            Pd_f = Pdk_f/sum(Pd_f)
+            Pd_r = np.exp(np.dot(eta,zbar_r))
+            Pdk_r = Pd_r[label_id_dict[label]]
+            Pd_r = Pdk_r/sum(Pd_r)
+            add = Pd_f/Pd_r
+            
+            pks = []
+            # compute the multinomial probability
+            for k in range(K):
+                pks += [((doc_topic_mat[int(news_id), k] + a)*(term_topic_mat[term_id, k] + 
+                         b))/(topic_mat[0,k]+b*T)*add]
+          #  pks = np.around(pks,6)
+            norm_cnst = sum(pks)
+            #Ndi = len(doc_term_dict[(news_id, term)]
+            k_Ndi_samples = np.random.multinomial(Ndi, pks/norm_cnst, size = 1)
+        
+        else: 
+            label = doc_label_dict[news_id]
+            Pd_f = np.exp(np.dot(eta,zbar_f))
+            Pdk_f = Pd_f[label_id_dict[label]]
+            Pd_f = Pdk_f/sum(Pd_f)
+            Pd_r = np.exp(np.dot(eta,zbar_r))
+            Pdk_r = Pd_r[label_id_dict[label]]
+            Pd_r = Pdk_r/sum(Pd_r)
+            add = Pd_f/Pd_r
+            
+            pks = []
+            for k in range(K):
+                pks += [((doc_topic_mat[int(news_id), k] + 
+                    a)*(term_topic_mat[term_id, k]+b))/(topic_mat[0,k]+b*T)*add]
+            norm_cnst = sum(pks)
+            #Ndi = len(doc_term_dict[(news_id, term)])
+            k_Ndi_samples = np.random.multinomial(Ndi, pks/norm_cnst, size = 1)
+
+        doc_term_dict_R31[(news_id, term)] = k_Ndi_samples
+        # update n_{doc, topic}, n_{term, topic} and n_{topic}
+        doc_topic_mat[int(news_id), :] = doc_topic_mat[int(news_id),:] + doc_term_dict_R31[(news_id, term)]
+        term_topic_mat[term_id, :] = term_topic_mat[term_id, :] + doc_term_dict_R31[(news_id, term)]
+        topic_mat[0,:] = topic_mat[0,:] + doc_term_dict_R31[(news_id, term)]
+
+    stop_loop1 = timeit.default_timer()
+    print(m, '-th loop, ', 'Time of d-loop: ', stop_loop1 - start_loop1)
+    # update phi (see pg.73 of http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf)
+    # this phi_{d,k} here looks like \theta_{d,z} in U Guleph tutorial
+    start_loop2 = timeit.default_timer()
+    for d1 in range(D):
+        news_id1 = docs_txt_lines2[d1]
+        sum_C_d_k = sum(doc_topic_mat[int(news_id1),:])
+        phi_sample[int(news_id1),:,m] = (doc_topic_mat[int(news_id1),:] + a)/(sum_C_d_k + K*a)  
+    stop_loop2 = timeit.default_timer()
+    print(m, '-th loop, ', 'Time of d1-loop: ', stop_loop2 - start_loop2)
+    # update theta (see pg.73 of http://proceedings.mlr.press/v13/xiao10a/xiao10a.pdf)
+    # this theta_{v,k} here looks like \phi_{w,z} (= \phi_{z,w}) in U Guleph tutorial
+    start_loop3 = timeit.default_timer()
+    for k1 in range(K):
+        sum_C_v_k = sum(term_topic_mat[:,k1])
+        theta_sample[:,k1,m] = (term_topic_mat[:,k1] + b)/(sum_C_v_k + T*b)
+    stop_loop3 = timeit.default_timer()
+    print(m, '-th loop, ', 'Time of k1-loop: ', stop_loop3 - start_loop3)
+stop_1 = timeit.default_timer()    
+print('Time: ', stop_1 - start_1)
+
+# __main__:51: RuntimeWarning: invalid value encountered in true_divide
+# may divide by zero or divide by NaN
+
+np.save("theta-local.npy", theta_sample)
+np.save("phi-local.npy", phi_sample)
+
+
+# 0 -th loop,  Time of d-loop:  346.15995464800017
+# 0 -th loop,  Time of d1-loop:  1.2260458130001552
+# 0 -th loop,  Time of k1-loop:  0.33909805700000106
+# 347.7262386130001
+
+
