c4b8fbe1ba69d134661c2183f058523ab0f4e03d
xcyang <xcyang@berkeley.edu>
2019-04-24 14:57:09 -0400

    finished writing log lik fn, kappa fn, and gradient / partial derivatives. now need to test code

--- 2nd-attempt/2topic-slda-prototype-omit-sanity-checks.py
+++ 2nd-attempt/2topic-slda-prototype-omit-sanity-checks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr  6 13:45:13 2019
+Created on Wed Apr 24 14:54:32 2019
 
 @author: apple
 """
+
 
 # import packages
 import numpy as np
